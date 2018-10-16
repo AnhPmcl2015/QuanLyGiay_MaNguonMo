743c3a810c98cc037a5cc96db44914ed898d119a
fangwohung <37786502+fangwohung@users.noreply.github.com>
2018-10-16 19:02:18 +0700

    Cấu hình server

---
+++ ProjectMaster/src/main/java/com/shoe/controller/DashboardController.java
@@ -0,0 +1,16 @@
+package com.shoe.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DashboardController {
+
+	@GetMapping
+	public ModelAndView welcome(Map<String, Object> model) {		
+		return new ModelAndView("dashboard");
+	}
+}
